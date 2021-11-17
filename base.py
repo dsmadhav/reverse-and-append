2ca535a65c7c2bdbfc9ba5a0c3f9430452caf9b7
dsmadhav <62181042+dsmadhav@users.noreply.github.com>
2021-11-17 12:36:34 +0530

    Update base.py

--- base.py
+++ base.py
@@ -1,10 +1,13 @@
 def a(i):
     i=i[::-1]
     return i
+#no. of words in the meme
 n=int(input())
 string[n]
+#take str input and append it to pre-existant str
 for i in range(n):
     string[i]=input()
     if(i>0):
         string[i]=string[i]+string[i-1]
+#print
 print(string)
